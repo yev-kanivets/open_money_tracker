8387659e8e8156b790997b0cf2098fe07ee32ba4
Evgeniy Kanivets <evgeniy.kanivets@coderivium.com>
2014-09-22 10:30:45 +0300

    Added sorting to report

--- app/src/main/java/com/blogspot/e_kanivets/moneytracker/model/Report.java
+++ app/src/main/java/com/blogspot/e_kanivets/moneytracker/model/Report.java
@@ -6,6 +6,8 @@ import com.blogspot.e_kanivets.moneytracker.R;
 import com.blogspot.e_kanivets.moneytracker.util.MTApp;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
@@ -47,17 +49,28 @@ public class Report {
         // Calculate total of all records
         int totalIncome = 0, totalExpense = 0;
 
-        //Convert HashMap to ArrayList
-        reportList = new ArrayList<Pair<String, Integer>>();
+        //Sort reportList
+        List<Pair<String, Integer>> reportIncomes = new ArrayList<Pair<String, Integer>>();
+        List<Pair<String, Integer>> reportExpenses = new ArrayList<Pair<String, Integer>>();
+
         for(String name : map.keySet()) {
-            reportList.add(new Pair<String, Integer>(name, map.get(name)));
             if(map.get(name) > 0) {
                 totalIncome += map.get(name);
+                reportIncomes.add(new Pair<String, Integer>(name, map.get(name)));
             }
             else {
                 totalExpense += map.get(name);
+                reportExpenses.add(new Pair<String, Integer>(name, map.get(name)));
             }
         }
+
+        sortList(reportIncomes);
+        sortList(reportExpenses);
+
+        //Added incomes and expenses to ArrayList
+        reportList = new ArrayList<Pair<String, Integer>>();
+        reportList.addAll(reportIncomes);
+        reportList.addAll(reportExpenses);
 
         //Add summary row to list
         reportList.add(new Pair<String, Integer>(
@@ -66,5 +79,19 @@ public class Report {
                 MTApp.get().getResources().getString(R.string.total_expenses) + " :", totalExpense));
         reportList.add(new Pair<String, Integer>(
                 MTApp.get().getResources().getString(R.string.total) + " :", totalExpense + totalIncome));
+    }
+
+    private void sortList(List<Pair<String, Integer>> list) {
+        int n = list.size();
+
+        for(int i=0;i<n-1;i++) {
+            for(int j=0;j<n-i-1;j++) {
+                if(Math.abs(list.get(j).second) < Math.abs(list.get(j+1).second)) {
+                    Pair<String, Integer> tmp = list.get(j);
+                    list.set(j, list.get(j+1));
+                    list.set(j+1, tmp);
+                }
+            }
+        }
     }
 }
