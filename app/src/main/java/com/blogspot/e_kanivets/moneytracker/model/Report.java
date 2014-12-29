953257488ec2e99e5d67abb67046b09ae6eecd31
evgenii <evgeniy.kanivets@coderivium.com>
2014-12-29 11:59:53 +0200

    Fixed wrong accounting of total's expense/income

--- app/src/main/java/com/blogspot/e_kanivets/moneytracker/model/Report.java
+++ app/src/main/java/com/blogspot/e_kanivets/moneytracker/model/Report.java
@@ -31,6 +31,9 @@ public class Report {
     private void makeReport() {
         HashMap<String, Integer> map = new HashMap<String, Integer>();
 
+        // Calculate total of all records
+        int totalIncome = 0, totalExpense = 0;
+
         for(Record record : records) {
             int sum = 0;
             //If category has already exist add to it
@@ -40,14 +43,13 @@ public class Report {
 
             //Add or subtract price
             if(record.isIncome()) {
+                totalIncome += record.getPrice();
                 map.put(record.getCategory(), sum + record.getPrice());
             } else {
+                totalExpense -= record.getPrice();
                 map.put(record.getCategory(), sum - record.getPrice());
             }
         }
-
-        // Calculate total of all records
-        int totalIncome = 0, totalExpense = 0;
 
         //Sort reportList
         List<Pair<String, Integer>> reportIncomes = new ArrayList<Pair<String, Integer>>();
@@ -55,11 +57,9 @@ public class Report {
 
         for(String name : map.keySet()) {
             if(map.get(name) > 0) {
-                totalIncome += map.get(name);
                 reportIncomes.add(new Pair<String, Integer>(name, map.get(name)));
             }
             else {
-                totalExpense += map.get(name);
                 reportExpenses.add(new Pair<String, Integer>(name, map.get(name)));
             }
         }
