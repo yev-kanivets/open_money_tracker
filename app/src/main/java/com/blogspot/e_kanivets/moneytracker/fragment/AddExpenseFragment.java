0a6877121a6cb5100b79a24ff65b9ddb44e0df12
evgenii <evgeniy.kanivets@coderivium.com>
2015-06-18 16:03:22 +0300

    Fixed a bug with wrong resizing of the layout after showing keyboard on small screens.

--- app/src/main/java/com/blogspot/e_kanivets/moneytracker/fragment/AddExpenseFragment.java
+++ app/src/main/java/com/blogspot/e_kanivets/moneytracker/fragment/AddExpenseFragment.java
@@ -1,8 +1,6 @@
 package com.blogspot.e_kanivets.moneytracker.fragment;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
@@ -13,8 +11,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -25,7 +21,6 @@ import com.blogspot.e_kanivets.moneytracker.activity.NavDrawerActivity;
 import com.blogspot.e_kanivets.moneytracker.helper.MTHelper;
 import com.blogspot.e_kanivets.moneytracker.model.Account;
 import com.blogspot.e_kanivets.moneytracker.model.Record;
-import com.blogspot.e_kanivets.moneytracker.util.AppUtils;
 
 import java.util.ArrayList;
 import java.util.Date;
