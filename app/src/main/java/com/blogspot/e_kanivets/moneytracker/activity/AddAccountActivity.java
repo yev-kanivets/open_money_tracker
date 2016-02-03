c375bc45b07a04c7dc3419b94c412332b25a0e5d
Evgenii Kanivets <evgenii.kanivets@gmail.com>
2016-02-03 15:01:46 +0200

    #5[30m]. Add currency spinner.

--- app/src/main/java/com/blogspot/e_kanivets/moneytracker/activity/AddAccountActivity.java
+++ app/src/main/java/com/blogspot/e_kanivets/moneytracker/activity/AddAccountActivity.java
@@ -1,13 +1,24 @@
 package com.blogspot.e_kanivets.moneytracker.activity;
 
+import android.support.v7.widget.AppCompatSpinner;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.SimpleAdapter;
 
 import com.blogspot.e_kanivets.moneytracker.R;
 import com.blogspot.e_kanivets.moneytracker.activity.base.BaseActivity;
 import com.blogspot.e_kanivets.moneytracker.controller.AccountController;
 import com.blogspot.e_kanivets.moneytracker.helper.DbHelper;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Currency;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Locale;
+import java.util.Set;
 
 import butterknife.Bind;
 
@@ -19,10 +30,20 @@ public class AddAccountActivity extends BaseActivity {
     EditText etTitle;
     @Bind(R.id.et_init_sum)
     EditText etInitSum;
+    @Bind(R.id.spinner)
+    AppCompatSpinner spinner;
 
     @Override
     protected int getContentViewId() {
         return R.layout.activity_add_account;
+    }
+
+    @Override
+    protected void initViews() {
+        super.initViews();
+
+        spinner.setAdapter(new ArrayAdapter<>(AddAccountActivity.this,
+                android.R.layout.simple_list_item_1, new ArrayList<>(getAllCurrencies())));
     }
 
     @Override
@@ -55,5 +76,27 @@ public class AddAccountActivity extends BaseActivity {
         int initSum = Integer.parseInt(etInitSum.getText().toString().trim());
 
         new AccountController(new DbHelper(AddAccountActivity.this)).addAccount(title, initSum);
+    }
+
+    public static List<String> getAllCurrencies() {
+        Set<Currency> toret = new HashSet<>();
+        Locale[] locs = Locale.getAvailableLocales();
+
+        for (Locale loc : locs) {
+            try {
+                toret.add(Currency.getInstance(loc));
+            } catch (Exception exc) {
+                // Locale not found
+            }
+        }
+
+        List<String> currencyList = new ArrayList<>();
+        for (Currency currency : toret) {
+            currencyList.add(currency.getCurrencyCode());
+        }
+
+        Collections.sort(currencyList);
+
+        return currencyList;
     }
 }
