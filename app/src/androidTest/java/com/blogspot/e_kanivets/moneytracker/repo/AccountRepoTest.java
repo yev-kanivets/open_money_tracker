c285c6d886b423bd90dd00123d47570c620f76df
Evgenii Kanivets <evgenii.kanivets@gmail.com>
2016-02-18 14:16:58 +0200

    Temp commit.

---
+++ app/src/androidTest/java/com/blogspot/e_kanivets/moneytracker/repo/AccountRepoTest.java
@@ -0,0 +1,16 @@
+package com.blogspot.e_kanivets.moneytracker.repo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by evgenii_kanivets on 2/17/16.
+ */
+public class AccountRepoTest {
+
+    @Test
+    public void testGetTable() throws Exception {
+
+    }
+}
---
+++ app/src/main/java/com/blogspot/e_kanivets/moneytracker/repo/AccountRepoTest.java
@@ -0,0 +1,51 @@
+package com.blogspot.e_kanivets.moneytracker.repo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by evgenii_kanivets on 2/17/16.
+ */
+public class AccountRepoTest {
+
+    @Test
+    public void testGetTable() throws Exception {
+
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+
+    }
+
+    @Test
+    public void testGetListFromCursor() throws Exception {
+
+    }
+
+    @Test
+    public void testRead() throws Exception {
+
+    }
+
+    @Test
+    public void testReadAll() throws Exception {
+
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+    }
+
+    @Test
+    public void testReadWithCondition() throws Exception {
+
+    }
+}
