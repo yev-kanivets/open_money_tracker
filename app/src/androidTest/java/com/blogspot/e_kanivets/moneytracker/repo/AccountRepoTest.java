6c8329f40bad5bea6a5a8ce29f0d3b518c7827bc
Evgenii Kanivets <evgenii.kanivets@gmail.com>
2016-03-01 11:32:07 +0200

    #17[1h]. Add tests for AccountRepo.

--- app/src/androidTest/java/com/blogspot/e_kanivets/moneytracker/repo/AccountRepoTest.java
+++ app/src/androidTest/java/com/blogspot/e_kanivets/moneytracker/repo/AccountRepoTest.java
@@ -1,16 +1,70 @@
 package com.blogspot.e_kanivets.moneytracker.repo;
 
-import org.junit.Test;
+import android.content.ContentValues;
+import android.database.Cursor;
 
-import static org.junit.Assert.*;
+import com.blogspot.e_kanivets.moneytracker.DbHelper;
+import com.blogspot.e_kanivets.moneytracker.model.Account;
+
+import junit.framework.TestCase;
+
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Created by evgenii_kanivets on 2/17/16.
+ * Android Test case.
+ * Created on 3/1/16.
+ *
+ * @author Evgenii Kanivets
  */
-public class AccountRepoTest {
+public class AccountRepoTest extends TestCase {
+    private AccountRepo repo;
 
-    @Test
+    public void setUp() throws Exception {
+        DbHelper mock = Mockito.mock(DbHelper.class);
+        repo = new AccountRepo(mock);
+    }
+
+    public void tearDown() throws Exception {
+        repo = null;
+    }
+
     public void testGetTable() throws Exception {
+        assertEquals(DbHelper.TABLE_ACCOUNTS, repo.getTable());
+    }
 
+    public void testContentValues() throws Exception {
+        Account account = new Account(-1, "title1", 100, "NON");
+
+        ContentValues expected = new ContentValues();
+        expected.put(DbHelper.TITLE_COLUMN, "title1");
+        expected.put(DbHelper.CUR_SUM_COLUMN, 100);
+        expected.put(DbHelper.CURRENCY_COLUMN, "NON");
+
+        ContentValues actual = repo.contentValues(account);
+
+        assertEquals(expected, actual);
+    }
+
+    public void testGetListFromCursor() throws Exception {
+        assertEquals(new ArrayList<Account>(), repo.getListFromCursor(Mockito.mock(Cursor.class)));
+
+        Cursor mockCursor = Mockito.mock(Cursor.class);
+        Mockito.when(mockCursor.moveToFirst()).thenReturn(true);
+        Mockito.when(mockCursor.getColumnIndex(DbHelper.ID_COLUMN)).thenReturn(1);
+        Mockito.when(mockCursor.getColumnIndex(DbHelper.TITLE_COLUMN)).thenReturn(2);
+        Mockito.when(mockCursor.getColumnIndex(DbHelper.CUR_SUM_COLUMN)).thenReturn(3);
+        Mockito.when(mockCursor.getColumnIndex(DbHelper.CURRENCY_COLUMN)).thenReturn(4);
+        Mockito.when(mockCursor.getLong(1)).thenReturn(1L);
+        Mockito.when(mockCursor.getString(2)).thenReturn("title");
+        Mockito.when(mockCursor.getInt(3)).thenReturn(100);
+        Mockito.when(mockCursor.getString(4)).thenReturn("NON");
+
+        List<Account> expected = new ArrayList<>();
+        expected.add(new Account(1, "title", 100, "NON"));
+
+        assertEquals(expected, repo.getListFromCursor(mockCursor));
     }
 }
