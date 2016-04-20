09d9315b2d4e3ab85f59ed26c64164c3601b2d5b
Evgenii Kanivets <evgenii.kanivets@gmail.com>
2016-04-20 11:29:29 +0300

    #34[30m]. Implement base cache on base of LruCache for CRUD operations.

--- app/src/main/java/com/blogspot/e_kanivets/moneytracker/repo/cache/BaseCache.java
+++ app/src/main/java/com/blogspot/e_kanivets/moneytracker/repo/cache/BaseCache.java
@@ -2,7 +2,9 @@ package com.blogspot.e_kanivets.moneytracker.repo.cache;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.util.LruCache;
 
+import com.blogspot.e_kanivets.moneytracker.entity.base.IEntity;
 import com.blogspot.e_kanivets.moneytracker.repo.base.IRepo;
 
 import java.util.List;
@@ -13,38 +15,54 @@ import java.util.List;
  *
  * @author Evgenii Kanivets
  */
-public class BaseCache<T> implements IRepo<T> {
+public class BaseCache<T extends IEntity> implements IRepo<T> {
     @SuppressWarnings("unused")
     private static final String TAG = "BaseCache";
+
+    private static final int CACHE_SIZE = 1024 * 1024; // 1MiB
+
+    private LruCache<Long, T> lruCache;
 
     @NonNull
     private IRepo<T> repo;
 
     public BaseCache(@NonNull IRepo<T> repo) {
         this.repo = repo;
+        lruCache = new LruCache<>(CACHE_SIZE);
     }
 
     @Nullable
     @Override
     public T create(@Nullable T instance) {
-        return repo.create(instance);
+        T createdT = repo.create(instance);
+        if (createdT != null) lruCache.put(createdT.getId(), createdT);
+        return createdT;
     }
 
     @Nullable
     @Override
     public T read(long id) {
-        return repo.read(id);
+        T cachedT = lruCache.get(id);
+        if (cachedT == null) {
+            T readT = repo.read(id);
+            if (readT != null) lruCache.put(readT.getId(), readT);
+            return readT;
+        } else return cachedT;
     }
 
     @Nullable
     @Override
     public T update(@Nullable T instance) {
-        return repo.update(instance);
+        T updatedT = repo.update(instance);
+        if (updatedT != null) lruCache.put(updatedT.getId(), updatedT);
+        return updatedT;
     }
 
     @Override
     public boolean delete(@Nullable T instance) {
-        return repo.delete(instance);
+        boolean deleted = repo.delete(instance);
+        if (instance != null && deleted) lruCache.remove(instance.getId());
+        return deleted;
     }
 
     @NonNull
