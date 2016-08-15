c56fb1f6d6f880cbce55f1915ba60e3aed04568c
Evgenii Kanivets <evgenii.kanivets@gmail.com>
2016-08-15 20:02:01 +0300

    #100[30m]. Add UI and base logic.

--- app/src/main/java/com/blogspot/e_kanivets/moneytracker/util/CategoryAutoCompleter.java
+++ app/src/main/java/com/blogspot/e_kanivets/moneytracker/util/CategoryAutoCompleter.java
@@ -4,7 +4,9 @@ import com.blogspot.e_kanivets.moneytracker.controller.data.CategoryController;
 import com.blogspot.e_kanivets.moneytracker.entity.data.Category;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Util class to encapsulate category autocomplete logic.
@@ -14,9 +16,11 @@ import java.util.List;
  */
 public class CategoryAutoCompleter {
     private List<String> categoryList;
+    private Set<String> filterSet;
 
     public CategoryAutoCompleter(CategoryController categoryController) {
         categoryList = new ArrayList<>();
+        filterSet = new HashSet<>();
 
         for (Category category : categoryController.readAll()) {
             categoryList.add(category.getName());
@@ -31,5 +35,10 @@ public class CategoryAutoCompleter {
         }
 
         return resultList;
+    }
+
+    public void removeFromAutoComplete(String category) {
+        filterSet.add(category);
+        categoryList.remove(category);
     }
 }
