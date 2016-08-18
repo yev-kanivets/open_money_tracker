6620e51eb32966dbd212d7fd969ee4e1316088b8
Evgenii Kanivets <evgenii.kanivets@gmail.com>
2016-08-18 19:08:12 +0300

    #107[1h]. Add about link and empty screen.

---
+++ app/src/main/java/com/blogspot/e_kanivets/moneytracker/activity/AboutActivity.java
@@ -0,0 +1,12 @@
+package com.blogspot.e_kanivets.moneytracker.activity;
+
+import com.blogspot.e_kanivets.moneytracker.R;
+import com.blogspot.e_kanivets.moneytracker.activity.base.BaseBackActivity;
+
+public class AboutActivity extends BaseBackActivity {
+
+    @Override
+    protected int getContentViewId() {
+        return R.layout.activity_about;
+    }
+}
