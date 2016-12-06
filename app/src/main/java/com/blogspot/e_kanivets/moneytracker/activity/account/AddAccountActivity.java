228f2ca2c53f5b44ebd6984b40c7b7256ae9cdfd
Evgenii Kanivets <evgenii.kanivets@gmail.com>
2016-12-07 00:35:32 +0200

    #70[1h 30m]. Add Account validation.

--- app/src/main/java/com/blogspot/e_kanivets/moneytracker/activity/account/AddAccountActivity.java
+++ app/src/main/java/com/blogspot/e_kanivets/moneytracker/activity/account/AddAccountActivity.java
@@ -3,14 +3,16 @@ package com.blogspot.e_kanivets.moneytracker.activity.account;
 import android.support.v7.widget.AppCompatSpinner;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 
 import com.blogspot.e_kanivets.moneytracker.R;
 import com.blogspot.e_kanivets.moneytracker.activity.base.BaseBackActivity;
 import com.blogspot.e_kanivets.moneytracker.controller.data.AccountController;
 import com.blogspot.e_kanivets.moneytracker.controller.CurrencyController;
 import com.blogspot.e_kanivets.moneytracker.entity.data.Account;
+import com.blogspot.e_kanivets.moneytracker.util.validator.AccountValidator;
+import com.blogspot.e_kanivets.moneytracker.util.validator.IValidator;
 
 import java.util.ArrayList;
 
@@ -27,10 +29,10 @@ public class AddAccountActivity extends BaseBackActivity {
     @Inject
     CurrencyController currencyController;
 
-    @Bind(R.id.et_title)
-    EditText etTitle;
-    @Bind(R.id.et_init_sum)
-    EditText etInitSum;
+    private IValidator<Account> accountValidator;
+
+    @Bind(R.id.content)
+    View contentView;
     @Bind(R.id.spinner)
     AppCompatSpinner spinner;
 
@@ -50,6 +52,7 @@ public class AddAccountActivity extends BaseBackActivity {
     protected void initViews() {
         super.initViews();
 
+        accountValidator = new AccountValidator(AddAccountActivity.this, contentView);
         spinner.setAdapter(new ArrayAdapter<>(AddAccountActivity.this,
                 R.layout.view_spinner_item,
                 new ArrayList<>(currencyController.readAll())));
@@ -57,7 +60,7 @@ public class AddAccountActivity extends BaseBackActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_add_record, menu);
+        getMenuInflater().inflate(R.menu.menu_add_account, menu);
         return true;
     }
 
@@ -68,7 +71,7 @@ public class AddAccountActivity extends BaseBackActivity {
                 if (addAccount()) {
                     setResult(RESULT_OK);
                     finish();
-                } else showToast(R.string.wrong_number_text);
+                }
                 return true;
 
             default:
@@ -76,20 +79,10 @@ public class AddAccountActivity extends BaseBackActivity {
         }
     }
 
+    @SuppressWarnings("SimplifiableIfStatement")
     private boolean addAccount() {
-        String title = etTitle.getText().toString().trim();
-
-        double initSum;
-        try {
-            initSum = Double.parseDouble(etInitSum.getText().toString().trim());
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-            return false;
-        }
-
-        String currency = (String) spinner.getSelectedItem();
-
-        Account account = new Account(title, initSum, currency);
-        return accountController.create(account) != null;
+        Account account = accountValidator.validate();
+        if (account == null) return false;
+        else return accountController.create(account) != null;
     }
 }
---
+++ app/src/main/java/com/blogspot/e_kanivets/moneytracker/util/validator/IValidator.java
@@ -0,0 +1,18 @@
+package com.blogspot.e_kanivets.moneytracker.util.validator;
+
+import android.support.annotation.Nullable;
+
+/**
+ * Interface of validators for data models. It may works with UI elements directly.
+ * Created on 06.12.2016.
+ *
+ * @author Evgenii Kanivets
+ */
+
+public interface IValidator<T> {
+    /**
+     * @return instance of class T if validation passed or null otherwise
+     */
+    @Nullable
+    T validate();
+}
