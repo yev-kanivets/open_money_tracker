e30f7af351accb66da5d7e2f8590d047af4e52ce
Fynn Godau <fynngodau@mailbox.org>
2019-08-28 15:58:21 +0200

    Remove dropbox dependency from free flavor

---
+++ app/src/free/java/com/blogspot/e_kanivets/moneytracker/activity/external/BackupActivity.java
@@ -0,0 +1,16 @@
+package com.blogspot.e_kanivets.moneytracker.activity.external;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * This class does nothing in the free flavor, but it is a lot easier to
+ * have it exist than to have it not exist.
+ */
+public class BackupActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/free/java/com/blogspot/e_kanivets/moneytracker/controller/backup/BackupController.java
@@ -0,0 +1,10 @@
+package com.blogspot.e_kanivets.moneytracker.controller.backup;
+
+import com.blogspot.e_kanivets.moneytracker.controller.FormatController;
+
+/** This class does nothing instead of Dropbox support. */
+public class BackupController {
+    public BackupController(FormatController formatController, String dataDir) {
+
+    }
+}
